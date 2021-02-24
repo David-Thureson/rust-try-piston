589b89d6db057ad0d8881fa65397062d8513661a
David Thureson <David.G.Thureson@gmail.com>
2021-02-23 16:53:24 -0800

    First commit.

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+
+pub mod getting_started;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use try_piston::getting_started;
+
+fn main() {
+    getting_started::main();
+}
